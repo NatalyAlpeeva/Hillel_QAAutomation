28162c76ad42d76ff1d7fe88ca9d34dbc577957f
NatalyAlpeeva <nataha.one@gmail.com>
2023-03-26 15:52:19 +0300

    added dz4

--- src/main/java/dz4.java
+++ src/main/java/dz4.java
@@ -9,12 +9,18 @@ public class dz4 {
             Arr[t] = rand.nextInt(20);
             System.out.println(Arr[t]);
         }
-        int sum=0;
+        int sum = 0;
         for (int j : Arr) {
             if (j > 5 && j < 45) {
-                sum=sum+j;
+                sum = sum + j;
             }
         }
         System.out.println("The sum is " + sum);
+        for (int t = 0; t < Arr.length; t++) {
+            if(Arr[t]%2==0) {
+                System.out.println(Arr[t] + "");
+            }
+        }
     }
 }
+
