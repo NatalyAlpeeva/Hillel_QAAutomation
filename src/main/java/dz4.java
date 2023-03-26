1c6f08c2efa1b1ba356db6de7003dac018d94682
NatalyAlpeeva <127839155+NatalyAlpeeva@users.noreply.github.com>
2023-03-26 15:25:42 +0300

    Add files via upload

---
+++ src/main/java/dz4.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.stream.*;
+
+public class dz4 {
+    public static void main(String[] args) {
+        int Arr[] = new int[20];
+        Random rand = new Random();
+        for (int t = 0; t < Arr.length; t++) {
+            Arr[t] = rand.nextInt(20);
+            System.out.println(Arr[t]);
+        }
+        int sum=0;
+        for (int j : Arr) {
+            if (j > 5 && j < 45) {
+                sum=sum+j;
+            }
+        }
+        System.out.println("The sum is " + sum);
+    }
+}
