930866dff2bb11d412368b1062317ead4201fd4a
NatalyAlpeeva <nataha.one@gmail.com>
2023-05-11 09:53:48 +0300

    added dz11

---
+++ src/main/java/dz11/Dz11.java
@@ -0,0 +1,15 @@
+package dz11;
+
+import java.util.*;
+
+public class Dz11 {
+    public static void main(String[] args) {
+        Map<Integer, String> intMap=new HashMap<>();
+       intMap.put(1, "value1");
+       intMap.put(2, "value2");
+       intMap.put(3, "value3");
+       intMap.put(4, "value4");
+       intMap.put(5, "value5");
+        System.out.println(intMap);
+    }
+}
