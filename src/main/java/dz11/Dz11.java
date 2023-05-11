0a56d3a4c4a5eb3e556825d2904ebd2178238faa
NatalyAlpeeva <nataha.one@gmail.com>
2023-05-11 10:10:07 +0300

    added dz11

--- src/main/java/dz11/Dz11.java
+++ src/main/java/dz11/Dz11.java
@@ -5,11 +5,18 @@ import java.util.*;
 public class Dz11 {
     public static void main(String[] args) {
         Map<Integer, String> intMap=new HashMap<>();
-       intMap.put(1, "value1");
-       intMap.put(2, "value2");
-       intMap.put(3, "value3");
-       intMap.put(4, "value4");
-       intMap.put(5, "value5");
+        intMap.put(1, "cat");
+        intMap.put(99, "dogs");
+        intMap.put(3, "tigers");
+        intMap.put(45, "squirrel");
+        intMap.put(100, "lion");
+        intMap.put(60, "bird");
+        intMap.put(2, "parrot");
+        intMap = sortByKeys(intMap);
         System.out.println(intMap);
+    }
+
+    private static Map<Integer, String> sortByKeys(Map<Integer, String> unsortedMap) {
+        return new TreeMap<>(unsortedMap);
     }
 }
